009fc919661174b794e1c1bb4a7a818e5eff4383
Lalan Kumar <21e51a6932@hitam.org>
2025-06-21 20:55:47 +0530

    Added Mlflow tracking

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -2,6 +2,8 @@ import os
 import sys
 from dataclasses import dataclass
 import tensorflow as tf
+import mlflow
+from mlflow.models import infer_signature
 
 # Dynamically add the project root directory to sys.path
 current_file_path = os.path.abspath(__file__)
@@ -22,6 +24,8 @@ class ModelTrainerConfig:
     model_save_path: str
     epochs: int
     batch_size: int
+    tracking_uri: str
+    experiment_name: str
     
 class ModelTrainer:
     def __init__(self):
@@ -29,21 +33,59 @@ class ModelTrainer:
         self.model_trainer_config= ModelTrainerConfig(
             model_save_path=self.config['model']['model_save_path'],
             epochs=self.config['model']['epochs'],
-            batch_size=self.config['model']['batch_size']
+            batch_size=self.config['model']['batch_size'],
+            tracking_uri=self.config['mlflow']['tracking_uri'],
+            experiment_name=self.config['mlflow']['experiment_name']
         )
         
     def initiate_model_training(self, X_train, y_train, X_val, y_val):
         try:
             logging.info("Entered Model Trainer Component")
             
+            mlflow.set_tracking_uri(uri=self.model_trainer_config.tracking_uri)
+            mlflow.set_experiment(experiment_name=self.model_trainer_config.experiment_name)
+            logging.info("MLflow tracking URI and experiment set successfully")
+            
             # Load the model
             model=build_model(input_shape=(X_train.shape[1], X_train.shape[2]))
-            logging.info("Model Loaded successfully")
+            logging.info("Model Built successfully")
             
-            # Train the model
-            history = model.fit(X_train, y_train, epochs=self.model_trainer_config.epochs, batch_size=self.model_trainer_config.batch_size, validation_data=(X_val, y_val))
-            logging.info("Model Training Completed")
-            
+            with mlflow.start_run():
+                
+                # Log model config
+                config = model.get_config()
+                mlflow.log_param("num_layers", len(config["layers"]))
+                mlflow.log_param("model_type", type(model).__name__)
+                mlflow.log_param("input_shape", str(model.input_shape))
+                mlflow.log_param("output_shape", str(model.output_shape))
+                mlflow.log_param("epochs", self.model_trainer_config.epochs)
+                mlflow.log_param("batch_size", self.model_trainer_config.batch_size)
+                logging.info("Model parameters logged successfully")
+                
+                # Log optimizer details
+                optimizer_config = model.optimizer.get_config()
+                mlflow.log_param("optimizer", optimizer_config["name"])
+                mlflow.log_param("learning_rate", optimizer_config["learning_rate"])
+                
+                # Train the model
+                history = model.fit(X_train, y_train, epochs=self.model_trainer_config.epochs, batch_size=self.model_trainer_config.batch_size, validation_data=(X_val, y_val))
+                logging.info("Model Training Completed")
+                
+                # Log final metrics
+                mlflow.log_metric("val_mae", history.history['val_mae'][-1])
+                mlflow.log_metric("val_loss", history.history['val_loss'][-1])
+                
+                # Infer the signature of the model
+                signature = infer_signature(X_train, model.predict(X_train))
+                logging.info("Model signature inferred successfully")
+                
+                # Log the model
+                mlflow.tensorflow.log_model(
+                    model=model,
+                    name="artifacts",
+                    signature=signature,
+                )
+                logging.info("Model logged to MLflow successfully")
             # Save the trained model locally
             save_keras_model(
                 save_path=self.model_trainer_config.model_save_path,
